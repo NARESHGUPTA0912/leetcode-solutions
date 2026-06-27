31dd71db0f3940c5a6b19c627c4d89b044d9509f
NARESH GUPTA <nareshgupta0912@gmail.com>
2026-06-27 18:19:21 +0530

    Time: 1 ms (100%), Space: 46.7 MB (56.29%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> res = new ArrayList<>();
+        int n = words.length;
+        for (int i = 0; i < n; i++)
+            if (words[i].indexOf(x) != -1)
+                res.add(i);
+        return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
