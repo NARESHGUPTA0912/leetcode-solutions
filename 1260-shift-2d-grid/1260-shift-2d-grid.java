1d6513d283f3f4ebe7608fe447926f9d9d594147
NARESH GUPTA <nareshgupta0912@gmail.com>
2026-06-27 19:23:40 +0530

    Time: 0 ms (100%), Space: 47.6 MB (7.87%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,29 +1,63 @@
+// class Solution {
+//     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         int total = m*n;
+//         int big = total*100;
+//         List<List<Integer>> res= new ArrayList<>();
+//         int[] a = new int[total];
+
+//         for(int i = 0;i<total;i++){
+//             a[i] = grid[i/n][i%n];
+//         }
+
+//         // for(int i =0;i<total;i++) {
+//         //     System.out.print(a[i]+" ");
+//         // }
+
+//         for(int i = 0;i<m;i++) {
+//             List<Integer> r= new ArrayList<>();
+//             for(int j =0;j<n;j++) {
+//                 r.add(a[(i*n+j+big - k) % total]);
+//             }
+//             res.add(r);
+//         }
+
+//         return res;
+//     }
+// }
+
+import java.util.AbstractList;
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int total = m*n;
-        int big = total*100;
-        List<List<Integer>> res= new ArrayList<>();
-        int[] a = new int[total];
+        final int n = grid.length;
+        final int m = grid[0].length;
+        final int total = n * m;
+        final int shift = k % total;
 
-        for(int i = 0;i<total;i++){
-            a[i] = grid[i/n][i%n];
-        }
+        return new AbstractList<List<Integer>>() {
+            @Override
+            public List<Integer> get(int i) {
+                return new AbstractList<Integer>() {
+                    @Override
+                    public Integer get(int j) {
+                        int target1D = i * m + j;
+                        int source1D = (target1D + total - shift) % total;
+                        return grid[source1D / m][source1D % m];
+                    }
 
-        // for(int i =0;i<total;i++) {
-        //     System.out.print(a[i]+" ");
-        // }
-
-        for(int i = 0;i<m;i++) {
-            List<Integer> r= new ArrayList<>();
-            for(int j =0;j<n;j++) {
-                r.add(a[(i*n+j+big - k) % total]);
+                    @Override
+                    public int size() {
+                        return m;
+                    }
+                };
             }
-            res.add(r);
-        }
 
-        return res;
+            @Override
+            public int size() {
+                return n;
+            }
+        };
     }
 }
 
