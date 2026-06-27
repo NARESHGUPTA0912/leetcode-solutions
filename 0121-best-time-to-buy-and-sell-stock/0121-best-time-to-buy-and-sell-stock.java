f642476d19f199d0dd02838ec9f4c4e6fdf0ffd9
NARESH GUPTA <nareshgupta0912@gmail.com>
2026-06-27 20:22:35 +0530

    Time: 1 ms (99.94%), Space: 94.4 MB (69.47%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice)
+                minPrice = price;
+            int profit = price - minPrice;
+            if (profit > maxProfit)
+                maxProfit = profit;
+        }
+        return maxProfit;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
