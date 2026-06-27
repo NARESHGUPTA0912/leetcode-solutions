610bdd5065c1d621c832dbfc4174319f10b604f2
NARESH GUPTA <nareshgupta0912@gmail.com>
2026-06-27 20:20:02 +0530

    Time: 0 ms (100%), Space: 43.1 MB (89.62%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        // case like [9,9,9] 
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;   
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
