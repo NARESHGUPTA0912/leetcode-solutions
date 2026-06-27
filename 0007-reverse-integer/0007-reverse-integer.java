7568f83de8f40e0647643fff3eaaff683f12d98f
NARESH GUPTA <nareshgupta0912@gmail.com>
2026-06-27 18:31:34 +0530

    Time: 1 ms (99.98%), Space: 42.7 MB (45.34%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,19 +1,32 @@
+// class Solution {
+//     public int reverse(int x) {
+//         long reversed = 0;
+
+//         while (x != 0) {
+//             reversed = reversed * 10 + x % 10;
+//             x /= 10;
+//             if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+//                 return 0; // overflow
+//             }
+//         }
+
+//         return (int) reversed;
+//     }
+// }
+
 class Solution {
     public int reverse(int x) {
-        long reversed = 0;
-
-        while (x != 0) {
-            reversed = reversed * 10 + x % 10;
-            x /= 10;
-            if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
-                return 0; // overflow
-            }
-        }
-
-        return (int) reversed;
+    int reverse = 0;
+    while(x!=0){
+        int lastDigit = x%10;
+        if(reverse > Integer.MAX_VALUE/10 || reverse < Integer.MIN_VALUE/10)
+            return 0;
+        reverse = reverse*10 + lastDigit;
+        x = x/10;
     }
+    return reverse;
+    }        
 }
-
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
