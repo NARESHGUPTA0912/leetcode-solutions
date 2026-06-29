8a82a3dd93f47b625631a14cb3ad0904bc4a4b78
NARESH GUPTA <nareshgupta0912@gmail.com>
2026-06-29 19:48:11 +0530

    Time: 1 ms (75.75%), Space: 43.2 MB (86.75%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,12 +1,18 @@
 class Solution {
+
     public int numOfStrings(String[] patterns, String word) {
         int res = 0;
-        for (String pattern : patterns)
-            if (check(pattern, word)) res++;
+        for (String pattern : patterns) {
+            if (check(pattern, word)) {
+                res++;
+            }
+        }
         return res;
     }
+
     private boolean check(String pattern, String word) {
-        int m = pattern.length(),   n = word.length();
+        int m = pattern.length();
+        int n = word.length();
         for (int i = 0; i + m <= n; i++) {
             boolean flag = true;
             for (int j = 0; j < m; j++) {
@@ -15,13 +21,13 @@ class Solution {
                     break;
                 }
             }
-            if (flag)
+            if (flag) {
                 return true;
-        } return false;
+            }
+        }
+        return false;
     }
 }
-
-
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
