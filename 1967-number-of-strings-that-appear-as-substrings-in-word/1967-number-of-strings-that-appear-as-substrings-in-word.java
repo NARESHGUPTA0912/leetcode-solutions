e1638580cb7ccecd7da53534e13a503ed8685791
NARESH GUPTA <nareshgupta0912@gmail.com>
2026-06-29 19:49:10 +0530

    Time: 2 ms (7.11%), Space: 43.3 MB (56.03%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,33 +1,48 @@
+// class Solution {
+
+//     public int numOfStrings(String[] patterns, String word) {
+//         int res = 0;
+//         for (String pattern : patterns) {
+//             if (check(pattern, word)) {
+//                 res++;
+//             }
+//         }
+//         return res;
+//     }
+
+//     private boolean check(String pattern, String word) {
+//         int m = pattern.length();
+//         int n = word.length();
+//         for (int i = 0; i + m <= n; i++) {
+//             boolean flag = true;
+//             for (int j = 0; j < m; j++) {
+//                 if (word.charAt(i + j) != pattern.charAt(j)) {
+//                     flag = false;
+//                     break;
+//                 }
+//             }
+//             if (flag) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
 class Solution {
-
     public int numOfStrings(String[] patterns, String word) {
-        int res = 0;
-        for (String pattern : patterns) {
-            if (check(pattern, word)) {
-                res++;
+        int n=patterns.length;
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(word.contains(patterns[i]))
+            {
+                count++;
             }
         }
-        return res;
-    }
-
-    private boolean check(String pattern, String word) {
-        int m = pattern.length();
-        int n = word.length();
-        for (int i = 0; i + m <= n; i++) {
-            boolean flag = true;
-            for (int j = 0; j < m; j++) {
-                if (word.charAt(i + j) != pattern.charAt(j)) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                return true;
-            }
-        }
-        return false;
+        return count;
     }
 }
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
