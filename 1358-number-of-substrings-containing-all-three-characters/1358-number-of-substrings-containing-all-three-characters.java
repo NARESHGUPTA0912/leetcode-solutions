8093dcefcabdd80ae15c763c99f3e03ff473da6d
NARESH GUPTA <nareshgupta0912@gmail.com>
2026-06-30 20:26:17 +0530

    Time: 12 ms (84.52%), Space: 46.5 MB (30.71%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,3 +1,16 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int len = s.length();
+        int[] lastPos = { -1, -1, -1 };     // Track last position of a, b, c
+        int total = 0;
+        for (int pos = 0; pos < len; pos++) {
+            // Update last position of current character
+            lastPos[s.charAt(pos) - 'a'] = pos; // Add count of valid substrings ending at current position  // If any character is missing, min will be -1  // Else min gives leftmost required character position
+            total += 1 + Math.min(lastPos[0], Math.min(lastPos[1], lastPos[2]));
+        } return total;
+    }
+}
+
 // class Solution {
 
 //     public int numberOfSubstrings(String s) {
@@ -33,25 +46,25 @@
 //     }
 // }
 
-class Solution {
-    public int numberOfSubstrings(String s) {
-        char[] ch = s.toCharArray();
-        int[] abc = new int[3];
-        for(int i = 0; i < abc.length; i++){
-            abc[i] = -1;
-        }
-        int count = 0, right = 0;
-        while(right < ch.length){
-            abc[ch[right] - 'a'] = right;
-            int minIndex = Integer.MAX_VALUE;
-            for(int i = 0; i < 3; i++)
-                minIndex = Math.min(minIndex, abc[i]);
-            count += (minIndex + 1);
-            right++;
-        }
-        return count;
-    }
-}
+// class Solution {
+//     public int numberOfSubstrings(String s) {
+//         char[] ch = s.toCharArray();
+//         int[] abc = new int[3];
+//         for(int i = 0; i < abc.length; i++){
+//             abc[i] = -1;
+//         }
+//         int count = 0, right = 0;
+//         while(right < ch.length){
+//             abc[ch[right] - 'a'] = right;
+//             int minIndex = Integer.MAX_VALUE;
+//             for(int i = 0; i < 3; i++)
+//                 minIndex = Math.min(minIndex, abc[i]);
+//             count += (minIndex + 1);
+//             right++;
+//         }
+//         return count;
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
